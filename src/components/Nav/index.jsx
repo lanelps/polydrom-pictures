334b8d1d82f4a3cb58d9bd224708aff8cad8389e
Lane Le Prevost-Smith <lanelps@gmail.com>
2022-09-07 11:28:17 +1200

    dark mode toggle button

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,16 +2,17 @@
 import React from "react";
 import tw from "twin.macro";
 
-import { Grid, Go } from "~components";
+import { Grid, Go, ThemeToggle } from "~components";
 
-const Container = tw.nav`absolute top-6 sm-t:top-4 w-full z-10`;
-const NavLink = tw.li`relative w-full col-span-full sm-t:col-span-2 sm-d:col-span-1`;
+const Container = tw.nav`absolute flex flex-col gap-y-4 top-6 sm-t:top-4 w-full z-10`;
+const NavLink = tw.li`relative w-full col-span-full sm-t:first:col-span-full sm-t:col-span-2 sm-t:first:col-span-1 sm-d:col-span-1`;
 const NavTitle = tw.h3`relative font-main text-m-h4 sm-t:text-d-h4 text-grey`;
 const Links = tw.ul`relative w-full`;
 const Link = tw.li`font-main font-main text-m-h4 sm-t:text-d-h4`;
 
 const Nav = ({ navLinks }) => (
   <Container>
+    <ThemeToggle />
     <Grid node="ul">
       {navLinks.map((navLink) => (
         <NavLink key={navLink?._key}>
---
+++ src/components/ThemeToggle/index.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useLayoutEffect, useEffect } from "react";
+import tw, { css, styled } from "twin.macro";
+
+import { ReactComponent as Dark } from "~assets/svg/dark.svg";
+import { ReactComponent as Light } from "~assets/svg/light.svg";
+
+const Container = styled.article(({ isDark }) => [
+  tw`relative sm-t:absolute sm-t:right-4 w-max flex gap-x-[0.11rem] sm-t:gap-x-[0.1875rem] p-[0.125rem] sm-t:p-1 ml-3 sm-t:m-0 rounded bg-offblack transition-colors z-10`,
+  isDark && tw`bg-offwhite`
+]);
+const ButtonWrapper = tw.button`relative w-[1.64rem] sm-t:w-10 h-[1.64rem] sm-t:h-10 flex items-center justify-center rounded`;
+
+const ThemeToggle = () => {
+  const [isDark, setIsDark] = useState();
+
+  useLayoutEffect(() => {
+    if (typeof window === `undefined`) return;
+
+    if (
+      localStorage.theme === `dark` ||
+      (!(`theme` in localStorage) &&
+        window.matchMedia(`(prefers-color-scheme: dark)`).matches)
+    ) {
+      // document.documentElement.classList.add('dark')
+      setIsDark(true);
+    } else {
+      // document.documentElement.classList.remove('dark')
+      setIsDark(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add(`dark`);
+    } else {
+      document.documentElement.classList.remove(`dark`);
+    }
+  }, [isDark]);
+
+  useEffect(() => {
+    console.log(`isDark`, isDark);
+  }, [isDark]);
+
+  return (
+    <Container isDark={isDark}>
+      <ButtonWrapper
+        type="button"
+        onClick={() => setIsDark(false)}
+        css={[tw`bg-offwhite`]}
+      >
+        <Light css={[tw`w-5 h-5 sm-t:w-8 sm-t:h-8 text-offblack`]} />
+      </ButtonWrapper>
+
+      <ButtonWrapper
+        type="button"
+        onClick={() => setIsDark(true)}
+        css={[tw`bg-offblack`]}
+      >
+        <Dark
+          css={[
+            tw`w-[0.423rem] sm-t:w-[0.625rem] h-[0.423rem] sm-t:h-[0.625rem] text-offwhite`
+          ]}
+        />
+      </ButtonWrapper>
+    </Container>
+  );
+};
+
+export default ThemeToggle;
