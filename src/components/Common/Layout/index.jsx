82215a4e5a2d8a9803b28d8b573a29f24c9355ed
Lane Le Prevost-Smith <lanelps@gmail.com>
2022-09-16 09:52:57 +1200

    Fixed footer to bottom of screen

--- src/components/Common/Layout/index.jsx
+++ src/components/Common/Layout/index.jsx
@@ -3,13 +3,16 @@ import tw from "twin.macro";
 
 import { Theme, Footer } from "~components";
 
-const Container = tw.div`relative h-screen flex bg-offwhite dark:bg-offblack transition-colors overflow-hidden`;
+const Container = tw.div`relative h-screen flex bg-offwhite dark:bg-offblack transition-colors overflow-x-hidden overflow-y-scroll`;
 
 const Layout = ({ children, className }) => (
   <>
     <Theme />
     <Container>
-      <main tw="relative w-full" className={className}>
+      <main
+        tw="relative w-full min-h-[667px] overflow-y-scroll"
+        className={className}
+      >
         {children}
       </main>
       <Footer />
