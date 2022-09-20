880a0f42f785a2f106cb570f6c13137dae6bb71c
Lane Le Prevost-Smith <lanelps@gmail.com>
2022-09-21 08:54:18 +1200

    theme locastorage changes

--- src/components/ThemeToggle/index.jsx
+++ src/components/ThemeToggle/index.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useLayoutEffect, useEffect } from "react";
-import tw, { css, styled } from "twin.macro";
+import React, { useLayoutEffect } from "react";
+import tw from "twin.macro";
 
 import { ReactComponent as Dark } from "~assets/svg/dark.svg";
 import { ReactComponent as Light } from "~assets/svg/light.svg";
@@ -8,8 +8,6 @@ const Container = tw.article`relative sm-t:absolute sm-t:right-4 w-max flex gap-
 const ButtonWrapper = tw.button`relative w-[1.64rem] sm-t:w-10 h-[1.64rem] sm-t:h-10 flex items-center justify-center rounded`;
 
 const ThemeToggle = () => {
-  const [isDark, setIsDark] = useState();
-
   useLayoutEffect(() => {
     if (typeof window === `undefined`) return;
 
@@ -18,27 +16,29 @@ const ThemeToggle = () => {
       (!(`theme` in localStorage) &&
         window.matchMedia(`(prefers-color-scheme: dark)`).matches)
     ) {
-      // document.documentElement.classList.add('dark')
-      setIsDark(true);
+      document.documentElement.classList.add(`dark`);
+      localStorage.setItem(`theme`, `dark`);
     } else {
-      // document.documentElement.classList.remove('dark')
-      setIsDark(false);
+      document.documentElement.classList.remove(`dark`);
+      localStorage.setItem(`theme`, `light`);
     }
   }, []);
 
-  useEffect(() => {
-    if (isDark) {
+  const handleThemetoggle = (theme) => {
+    if (theme === `dark`) {
       document.documentElement.classList.add(`dark`);
+      localStorage.setItem(`theme`, `dark`);
     } else {
       document.documentElement.classList.remove(`dark`);
+      localStorage.setItem(`theme`, `light`);
     }
-  }, [isDark]);
+  };
 
   return (
-    <Container isDark={isDark}>
+    <Container>
       <ButtonWrapper
         type="button"
-        onClick={() => setIsDark(false)}
+        onClick={() => handleThemetoggle(`light`)}
         css={[tw`bg-offwhite`]}
       >
         <Light css={[tw`w-5 h-5 sm-t:w-8 sm-t:h-8 text-offblack`]} />
@@ -46,7 +46,7 @@ const ThemeToggle = () => {
 
       <ButtonWrapper
         type="button"
-        onClick={() => setIsDark(true)}
+        onClick={() => handleThemetoggle(`dark`)}
         css={[tw`bg-offblack`]}
       >
         <Dark
