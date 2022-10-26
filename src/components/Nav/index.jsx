2127e670014ca0c69a0586888f861e684749dc6e
Lane Le Prevost-Smith <lanelps@gmail.com>
2022-10-27 12:54:26 +1300

    tw first-of-type

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -10,7 +10,7 @@ const Container = styled.nav(() => [
     transform: translateY(calc(-100% - 1rem));
   `
 ]);
-const NavLink = tw.li`relative w-full col-span-full sm-t:first:col-span-full sm-t:col-span-2 sm-d:first:col-span-1 sm-d:col-span-1`;
+const NavLink = tw.li`relative w-full col-span-full sm-t:first-of-type:col-span-full sm-t:col-span-2 sm-d:first-of-type:col-span-1 sm-d:col-span-1`;
 const NavTitle = tw.h3`relative font-main text-m-h4 sm-t:text-d-h4 text-grey`;
 const Links = tw.ul`relative w-full`;
 const Link = tw.li`font-main font-main text-m-h4 sm-t:text-d-h4 text-offblack dark:text-offwhite transition-colors`;
