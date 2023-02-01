1b5635a7c9306a279367571ccd731808e3c4d2a6
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-02-01 15:18:59 +1300

    maxWidth updated to 1728px

--- src/components/Common/Grid/index.jsx
+++ src/components/Common/Grid/index.jsx
@@ -12,7 +12,7 @@ const Grid = forwardRef((props, ref) => {
     gap = [`1rem`, `1rem`],
     padding = [`0 0.75rem`, `0 1rem`],
     margin = [`auto`, `auto`],
-    maxWidth = `1440px`
+    maxWidth = `1728px`
   } = props;
 
   const G = `${node}`;
