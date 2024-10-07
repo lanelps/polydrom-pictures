56e97efef304676a8d42ee0029f920babaae8bf1
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-10-07 12:38:30 +1100

    Refactor About component to optimize rendering and improve user experience

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,80 +1,99 @@
 import React, { useEffect, useRef, useCallback } from "react";
 import tw, { css, styled } from "twin.macro";
 
-import { Grid, PortableText } from "~components";
+import { PortableText } from "~components";
 import { useApp, useSize, useZIndex } from "~hooks";
 
-import { deviceType } from "~utils/helpers";
-
 const Container = styled.section(({ active }) => [
-  tw`absolute top-0 bottom-0 left-0 right-0 py-4 pointer-events-none overflow-hidden opacity-100 transition-opacity delay-[300ms]`,
-  active && tw`opacity-100 delay-[0ms]`,
+  tw`absolute top-0 bottom-0 left-0 right-0 pointer-events-none overflow-hidden opacity-100 transition-opacity delay-[300ms]`,
+  active && tw`opacity-100 delay-[0ms]`
 ]);
 
 const TransformWrapper = styled.div(({ active }) => [
-  tw`relative w-[calc(100% + 1.5rem)] sm-t:w-[calc(100% + 1rem)] h-[90%] sm-t:h-[85%] col-span-full sm-t:col-start-2 sm-t:col-span-3 self-end translate-x-full sm-t:translate-x-[calc(100% + (100%/3))] translate-y-4 transition-transform duration-[600ms] overflow-hidden`,
-  active && tw`-translate-x-3 sm-t:translate-x-1/3`
+  tw`absolute bottom-0 w-full h-[90%] sm-t:h-[50vh] translate-y-full transition-transform duration-[600ms] overflow-hidden`,
+  active && tw`translate-y-0`
 ]);
 
 const Background = styled.div(() => [
-  tw`w-full h-full absolute p-3 sm-t:p-4 pb-24 sm-t:pb-16 sm-d:pb-[4.75rem] bg-green dark:bg-red transition-colors z-[1]`,
+  tw`w-full h-full absolute p-3 sm-t:p-4 pb-24 sm-t:pb-16 sm-d:pb-[4.75rem] bg-green dark:bg-yellow transition-colors z-[1]`,
   css`
-    clip-path: inset(0px 0px 0px 0px);
+    mask: url(#holeMask) no-repeat center;
+    mask-size: cover;
   `
 ]);
 
-const Content = styled.div(({ active }) => [
-  tw`w-full h-full relative p-3 sm-t:p-4 pb-24 sm-t:pb-16 sm-d:pb-[4.75rem] overflow-y-scroll overflow-x-hidden z-10 pointer-events-none text-offblack dark:text-offwhite transition-colors`,
-  active && tw`pointer-events-auto`
+const BaseContent = styled.div(() => [
+  tw`w-full h-full p-3 sm-t:p-4 pb-24 sm-t:pb-16 sm-d:pb-[4.75rem] overflow-y-scroll overflow-x-hidden transition-colors pointer-events-auto`
 ]);
 
-const Circle = React.memo(
-  styled.div(({ size }) => [
-    tw`hidden sm-t:block fixed bg-babyblue dark:bg-offblack rounded-full z-[5] -translate-x-full -translate-y-full`,
-    css`
-      width: ${size}px;
-      height: ${size}px;
-    `
-  ])
-);
+const MaskedContent = styled(BaseContent)(() => [
+  tw`absolute inset-0 text-offblack dark:text-offwhite`
+]);
+
+const MainContent = styled(BaseContent)(() => [
+  tw`relative z-10 text-offblack`,
+  css`
+    mask: url(#holeMask) no-repeat center;
+    mask-size: cover;
+  `
+]);
 
 // Utility: Throttle function
-const throttle = (func) => {
+const throttle = (func, limit) => {
   let inThrottle;
   return function (...args) {
     if (!inThrottle) {
       func.apply(this, args);
       inThrottle = true;
-      requestAnimationFrame(() => (inThrottle = false));
+      setTimeout(() => (inThrottle = false), limit);
     }
   };
 };
 
+const SvgMask = React.memo(
+  React.forwardRef(({ size }, ref) => (
+    <svg tw="absolute w-full h-full">
+      <defs>
+        <mask id="holeMask">
+          <rect width="100%" height="100%" fill="white" />
+          <circle
+            ref={ref}
+            cx="0"
+            cy="0"
+            r={size / 2}
+            fill="black"
+            tw="hidden sm-t:block"
+          />
+        </mask>
+      </defs>
+    </svg>
+  ))
+);
+
 const About = ({ body }) => {
   const { isWindowActive, activeWindows } = useApp();
-  const aboutActive = isWindowActive('about');
+  const aboutActive = isWindowActive("about");
 
   const [backgroundRef, backgroundSize] = useSize();
 
   // Refs for mutable values
   const positionRef = useRef({ x: 0, y: 0 });
   const offsetRef = useRef({ x: 0, y: 0 });
-  const circleRef = useRef(null);
+  const maskCircleRef = useRef(null);
   const containerRef = useRef(null);
 
-  const size = 718;
-  const showRef = useRef(deviceType() === "desktop");
+  const size = 388;
 
   const zIndex = useZIndex("about");
 
- // Function to update z-index directly on the DOM element
- const updateZIndex = useCallback(() => {
-  if (containerRef.current) {
-    containerRef.current.style.zIndex = zIndex;
-  }
-}, [zIndex]);
+  // Function to update z-index directly on the DOM element
+  const updateZIndex = useCallback(() => {
+    if (containerRef.current) {
+      containerRef.current.style.zIndex = zIndex;
+    }
+  }, [zIndex]);
 
-  // Handle mouse move with throttling
+  // Handle mouse move with throttling and requestAnimationFrame
   const handleMouseMove = useCallback(
     throttle((e) => {
       positionRef.current = { x: e.clientX, y: e.clientY };
@@ -83,15 +102,19 @@ const About = ({ body }) => {
         offsetRef.current = { x: left, y: top };
       }
 
-      // Update Circle position directly
-      if (circleRef.current) {
-        const { x, y } = positionRef.current;
-        const { x: offsetX, y: offsetY } = offsetRef.current;
-        circleRef.current.style.transform = `translate3d(${x - offsetX - size / 2}px, ${
-          y - offsetY - size / 2
-        }px, 0)`;
+      const { x, y } = positionRef.current;
+      const { x: offsetX, y: offsetY } = offsetRef.current;
+
+      const relativeX = x - offsetX;
+      const relativeY = y - offsetY;
+
+      if (maskCircleRef.current) {
+        requestAnimationFrame(() => {
+          maskCircleRef.current.setAttribute("cx", relativeX);
+          maskCircleRef.current.setAttribute("cy", relativeY);
+        });
       }
-    }, 16), // Approximately 60fps
+    }, 16), // Approximately 60fps, adjust as needed
     [backgroundRef, size]
   );
 
@@ -119,23 +142,19 @@ const About = ({ body }) => {
   }, [backgroundRef, backgroundSize, aboutActive]);
 
   return (
-  <Container ref={containerRef} active={aboutActive}>
-    <Grid css={[tw`h-full`]}>
-        <TransformWrapper active={aboutActive}>
-          <Background ref={backgroundRef}>
-            {showRef.current && (
-              <Circle
-                ref={circleRef}
-                size={size}
-              />
-            )}
-          </Background>
+    <Container ref={containerRef} active={aboutActive}>
+      <SvgMask ref={maskCircleRef} size={size} />
+      <TransformWrapper active={aboutActive}>
+        <MaskedContent active={aboutActive} aria-hidden="true">
+          <PortableText blocks={body} />
+        </MaskedContent>
 
-          <Content active={aboutActive}>
-            <PortableText blocks={body} css={[tw`z-10`]} />
-          </Content>
-        </TransformWrapper>
-      </Grid>
+        <Background ref={backgroundRef} />
+
+        <MainContent active={aboutActive}>
+          <PortableText blocks={body} />
+        </MainContent>
+      </TransformWrapper>
     </Container>
   );
 };
