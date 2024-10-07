913bfc54f522cb0d8daaf91c016a45a9f5e83a19
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-10-07 11:02:37 +1100

    adjusting Button styles

--- src/components/Common/Button/index.jsx
+++ src/components/Common/Button/index.jsx
@@ -4,11 +4,11 @@ import tw, { css, styled } from "twin.macro";
 import { Go } from "~components";
 
 const Container = styled.button(({ buttonType }) => [
-  tw`relative w-max p-1 sm-t:p-2 sm-t:pb-1.5 bg-transparent border border-offblack dark:(text-offwhite border-offwhite) disabled:border-grey disabled:text-grey disabled:pointer-events-none disabled:opacity-50 transition-colors`,
+  tw`relative w-max disabled:text-grey disabled:pointer-events-none disabled:opacity-50 transition-colors`,
   buttonType === 1 &&
-    tw`text-offblack hover:(bg-offblack text-offwhite) dark:hover:(bg-offwhite text-offblack border-offblack) active:(bg-transparent text-offblack/60 border-offblack/60) dark:active:(bg-transparent text-offwhite/60 border-offwhite/60)`,
+    tw`text-offblack/60 dark:(text-offwhite/60) hover:(text-offwhite) dark:hover:(text-offwhite) active:(text-offblack) dark:active:(text-offwhite)`,
   buttonType === 2 &&
-    tw`text-offblack rounded-[3.5rem] uppercase hover:(bg-offblack text-offwhite border-offwhite)`
+    tw`p-1 sm-t:p-2 sm-t:pb-1.5 bg-transparent rounded-[3.5rem] uppercase text-offblack border border-offblack dark:(text-offwhite border-offwhite) hover:(bg-offblack text-offwhite border-offwhite) dark:hover:(bg-offwhite text-offblack border-offblack) active:(bg-offblack/60) dark:active:(bg-offwhite/60) disabled:border-grey`
 ]);
 
 const Button = ({
