a0f349436d653974ddf4286d5fc224843318aa69
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-11-07 16:31:22 +1100

    Update Layout background color to black and enhance VimeoVideo component with placeholder image

--- src/components/Common/VimeoVideo/index.jsx
+++ src/components/Common/VimeoVideo/index.jsx
@@ -3,14 +3,21 @@ import tw, { styled, css } from "twin.macro";
 import Player from "@vimeo/player";
 
 import { useApp } from "~hooks";
+import { Image } from "~components";
+
+import logo from "../../../../static/polydrom-white.png";
 
 const Container = styled.div(({ active }) => [
   tw`relative w-full h-full overflow-hidden opacity-0 transition-opacity delay-300`,
   active && tw`opacity-100`
 ]);
 
+const PlaceHolder = styled.img(() => [
+  tw`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ml-[2.5vw] sm-t:ml-0 sm-t:-mt-[2.5vw] object-contain w-[140%] sm-t:w-[60%] h-[100vw] sm-t:h-[100dvh] transition-opacity duration-1000 -rotate-90 sm-t:rotate-0 max-w-none pointer-events-none select-none`
+]);
+
 const VideoElement = styled.div(() => [
-  tw`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ml-[5vw] sm-t:ml-0 scale-[1.5] sm-t:scale-100 object-contain object-[65%_50%] sm-t:object-[50%_50] sm-t:object-cover w-[100dvh] sm-t:w-screen h-[100vw] sm-t:h-[100dvh] transition-opacity duration-1000 -rotate-90 sm-t:rotate-0 max-w-none pointer-events-none select-none`,
+  tw`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ml-[3vw] -mt-[5vw] sm-t:mt-0 sm-t:ml-0 scale-[1.2] sm-t:scale-[1.07] object-contain object-[65%_50%] sm-t:object-[50%_50] sm-t:object-cover w-[100dvh] sm-t:w-screen h-[100vw] sm-t:h-[100dvh] transition-opacity duration-1000 -rotate-90 sm-t:rotate-0 max-w-none pointer-events-none select-none bg-transparent`,
   css`
     & iframe {
       width: 100%;
@@ -72,6 +79,7 @@ const VimeoVideo = ({ source }) => {
 
   return (
     <Container active={isPlaying}>
+      <PlaceHolder src={logo} />
       <VideoElement ref={ref} id="vimeo-content" />
     </Container>
   );
